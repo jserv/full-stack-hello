d07753710c15a06359141b8f85e336fe89cd2400
Louie Lu <git@louie.lu>
2017-05-24 16:45:00 +0800

    Fix test runner and add comment to halt test

--- tests/runner.py
+++ tests/runner.py
@@ -12,9 +12,13 @@ class TestRunner(unittest.TestCase):
         output = self.get_output('hello.s')
         self.assertEqual(output, b'Hello\n')
 
-    def test_test(self):
-        output = self.get_output('test.s')
+    def test_coverage(self):
+        output = self.get_output('coverage.s')
         self.assertEqual(output, b'42\n50\n150\n')
+
+    def test_halt(self):
+        output = self.get_output('halt.s')
+        self.assertEqual(output, b'42\n')
 
 
 if __name__ == '__main__':
