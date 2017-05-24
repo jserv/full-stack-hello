57d7dd153471830f8e91c95d74a4d5ff5f4fae5c
Jim Huang <jserv.tw@gmail.com>
2017-05-24 19:06:02 +0800

    Fix typo

--- tests/halt.s
+++ tests/halt.s
@@ -2,4 +2,4 @@ add $40 $2 #1
 add #1 $8 #2
 print #1
 halt      ; This should halt the program
-print #2  ; And thus this line should not be execute
+print #2  ; And thus this instruction should not be executed
