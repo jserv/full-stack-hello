2b32d0473f065375f50c758917f24e86712ec99c
Louie Lu <git@louie.lu>
2017-05-24 18:31:19 +0800

    Refactor test runner
    Add metaclass for testrunner to auto generate base test case
    
    Now test file and expected result is seperate at .s and
    .expected files.

--- tests/runner.py
+++ tests/runner.py
@@ -1,24 +1,42 @@
 # -*- coding: utf-8 -*-
 
+import os
+import glob
 import subprocess
 import unittest
 
 
-class TestRunner(unittest.TestCase):
-    def get_output(self, testfile):
-        return subprocess.check_output(['./as_exec', 'tests/%s' % (testfile)])
+testfile = map(lambda x: x.strip('.s'), glob.glob('tests/*.s'))
 
-    def test_hello(self):
-        output = self.get_output('hello.s')
-        self.assertEqual(output, b'Hello\n')
 
-    def test_coverage(self):
-        output = self.get_output('coverage.s')
-        self.assertEqual(output, b'42\n50\n150\n')
+def get_expect_output(testfile):
+    with open('%s.expected' % testfile, 'rb') as f:
+        return f.read()
 
-    def test_halt(self):
-        output = self.get_output('halt.s')
-        self.assertEqual(output, b'42\n')
+
+def get_exec_output(testfile):
+    return subprocess.check_output(['./as_exec', '%s.s' % testfile])
+
+
+class TestRunnerMeta(type):
+
+    def __new__(mcs, name, bases, dict):
+        def gen_test(got, expected):
+            def test(self):
+                self.assertEqual(got, expected)
+            return test
+
+        for f in testfile:
+            test_name = 'test_%s' % (os.path.basename(f))
+            got = get_exec_output(f)
+            expected = get_expect_output(f)
+            dict[test_name] = gen_test(got, expected)
+
+        return type.__new__(mcs, name, bases, dict)
+
+
+class TestRunner(unittest.TestCase, metaclass=TestRunnerMeta):
+    pass
 
 
 if __name__ == '__main__':
