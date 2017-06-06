8c0ffd2ba67af6fdfb0d5321e376b7b635d5f6e6
Louie Lu <git@louie.lu>
2017-06-06 12:53:36 +0800

    Change label test to 10! result

--- tests/label.s
+++ tests/label.s
@@ -1,9 +1,9 @@
 ; Test for label
-; Output 1 + 2 + ... + 10 result
-add $0 $0 #0    ; result
+; Output 10! result
+add $0 $1 #0    ; result
 add $0 $10 #1   ; start from 10
 go:
-add #0 #1 #0    ; result = result + n
+mul #0 #1 #0    ; result = result + n
 sub #1 $1 #1    ; n = n - 1
 jnz #1 :go      ; jump to go if #1 not zero
 
@@ -11,6 +11,6 @@ jmp :output       ; jump to output
 add $0 $100 #1    ; should not be executed, #1 remained 0
 ; print the result
 output:
-print "1 + 2 + 3 + ... + 10 ="
-print #0        ; check result is 55
+print "10! ="
+print #0        ; check result is 3628800
 print #1        ; check #1 is 0
