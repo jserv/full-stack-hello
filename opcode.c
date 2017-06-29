5384ae084af77acce7248d7f5376bc149551c467
anthony liu <antliu@gmail.com>
2017-06-29 15:20:42 +0800

    Add bitwise operations support.

--- opcode.c
+++ opcode.c
@@ -53,6 +53,69 @@ static void mod_impl(VM_HANDLER_ARGS)
     }
 }
 
+static void and_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op1) == INT) {
+        VM_INT(result) = VM_INT(op1) & VM_INT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void or_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op1) == INT) {
+        VM_INT(result) = VM_INT(op1) | VM_INT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void not_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT) {
+        VM_INT(op2) = ~VM_INT(op1);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void xor_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op2) == INT) {
+        VM_INT(result) = VM_INT(op1) ^ VM_INT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void lsl_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op2) == INT) {
+        VM_INT(result) = VM_UINT(op1) << VM_UINT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void lsr_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op2) == INT) {
+        VM_INT(result) = VM_UINT(op1) >> VM_UINT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
+static void asr_impl(VM_HANDLER_ARGS)
+{
+    if (VM_T(op1) == INT && VM_T(op2) == INT) {
+        VM_INT(result) = VM_INT(op1) >> VM_UINT(op2);
+    } else {
+        UNIMPL;
+    }
+}
+
 static void print_impl(VM_HANDLER_ARGS)
 {
     switch (VM_T(op1)) {
@@ -75,4 +138,11 @@ void hook_opcodes(vm_env *env)
     vm_hook_opcode_handler(env, OP_MUL, mul_impl);
     vm_hook_opcode_handler(env, OP_DIV, div_impl);
     vm_hook_opcode_handler(env, OP_MOD, mod_impl);
+    vm_hook_opcode_handler(env, OP_AND, and_impl);
+    vm_hook_opcode_handler(env, OP_OR, or_impl);
+    vm_hook_opcode_handler(env, OP_NOT, not_impl);
+    vm_hook_opcode_handler(env, OP_XOR, xor_impl);
+    vm_hook_opcode_handler(env, OP_LSL, lsl_impl);
+    vm_hook_opcode_handler(env, OP_LSR, lsr_impl);
+    vm_hook_opcode_handler(env, OP_ASR, asr_impl);
 }
--- tests/coverage.s
+++ tests/coverage.s
@@ -15,10 +15,72 @@ div $4 $2 #6            ; divide: 4 / 2 = 2
 mod $5 $2 #7            ; modulo: 5 % 2 = 1
 print #6
 print #7
-call #18
+call #19
+jmp #24
 halt
 print $66
-call #21
+call #22
 ret
 print $77
 ret
+;
+; AND (bitwise AND)
+;
+; 3 & 1 = 1
+;
+and $3 $1 #8
+print #8
+;
+; OR (bitwise OR)
+;
+; 2 | 0 = 2
+;
+or $2 $0 #9
+print #9
+;
+; NOT (negate all bits)
+;
+; ~(-2) = 1
+;
+sub $0 $2 #10
+not #10 #11
+print #11
+;
+; XOR (exclusive OR)
+;
+; (-2) ^ 1 = -1
+;
+xor #10 #11 #12
+print #12
+;
+; LSL (logical shift left)
+;
+; ~(0) = -1
+; -1 << 1 = -2
+;
+not $0 #13
+lsl #13 $1 #13
+print #13
+;
+; LSR (logical shift right)
+;
+; ~(0) = -1
+; -1 >> 1 = MaxInt
+;
+not $0 #13
+lsr #13 $1 #13
+print #13
+;
+; ASR (arithmetic shift right)
+;
+; ~(0) = -1
+; -1 >> 1 (LSR) = MaxInt
+; ~(MaxInt) = MinInt
+; MinInt >> 1 = 2b'110....0
+;
+not $0 #13
+lsr #13 $1 #13
+not #13 #13
+asr #13 $1 #13
+print #13
+halt
